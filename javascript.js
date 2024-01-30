0e3e540b216d8ac04854e2ccfbc3a2c289e713e2
Colby Ramsay <149699778+colbyramsay@users.noreply.github.com>
2024-01-29 23:23:43 -0800

    Allow user to input books, display book info & array of books

--- javascript.js
+++ javascript.js
@@ -10,29 +10,51 @@ function Book(title, author, pages, read) {
     }
 }
 
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'already read');
+const book0 = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'Y');
 
-function addBookToLibrary() {
-  // do stuff here
-}
-
-myLibrary[0] = book1;
+myLibrary[0] = book0;
 
 const container = document.querySelector('#container');
 
 const content = document.createElement('div');
 content.classList.add('content');
-content.textContent = `${myLibrary[0].title} by ${myLibrary[0].author}`;
+content.textContent = `${myLibrary[0].title} by ${myLibrary[0].author}, ${myLibrary[0].pages} pages`;
 
 container.appendChild(content);
+
+let i = 1;
 
 document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault();
     let bookTitle = document.getElementById("title").value;
-    /*alert("The book title is " + bookTitle + "!");*/
-    const book2 = new Book(bookTitle);
-    myLibrary[1] = book2;
-    alert(myLibrary[1].title);
+    let bookAuthor = document.getElementById("author").value;
+    let bookPages = document.getElementById("pages").value;
+    let book = new Book(bookTitle, bookAuthor, bookPages);
+    myLibrary[i] = book;
     console.table(myLibrary);
-
+    myForm.reset();
+    const content1 = document.createElement('div');
+    content1.classList.add('content');
+    content1.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages} pages`;
+    container.appendChild(content1);
+    i+=1;
 });
+
+document.getElementById("myButton").addEventListener("click", function (event) {
+    //event.preventDefault();
+    const container2 = document.querySelector('#container2');
+    container2.innerHTML = "";
+    
+    for (i = 0; i < myLibrary.length; i++) {
+    
+    const content2 = document.createElement('div');
+    content2.classList.add('content');
+    content2.textContent = `${myLibrary[i].title}`;
+
+    container2.appendChild(content2);
+    }
+});
+
+     
+
+    
